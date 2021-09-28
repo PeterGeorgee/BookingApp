29422ae0560ca318d175bbed208e84dbd52397e6
abadeerafif <77312611+abadeerafif@users.noreply.github.com>
2021-09-28 11:18:04 +0200

    Update wrapper.dart

--- lib/screens/home/wrapper.dart
+++ lib/screens/home/wrapper.dart
@@ -2,8 +2,11 @@ import 'package:booking_app/models/user.dart';
 import 'package:booking_app/screens/authentication/authenticate.dart';
 import 'package:booking_app/screens/datapages/properties_list.dart';
 import 'package:booking_app/screens/datapages/properties_listt.dart';
+import 'package:booking_app/services/GetPropdata.dart';
+import 'package:booking_app/services/properties_class.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
@@ -16,7 +19,8 @@ class Wrapper extends StatelessWidget {
       return Authenticate();
     }
     else{
-      return PropertiesList();
+      return StreamProvider<List<Properties>>.value(child: PropertiesList(),
+      value: getpropertiesfromdatabase().propers, initialData: [],);
       //return PropsList();
     }
   }
