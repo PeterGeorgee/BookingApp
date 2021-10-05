7adc41beedc8f85e5096228a6c606d0feba12500
abadeerafif <77312611+abadeerafif@users.noreply.github.com>
2021-10-05 18:34:04 +0200

    Update GetPropdata.dart

--- lib/services/GetPropdata.dart
+++ lib/services/GetPropdata.dart
@@ -1,3 +1,6 @@
+
+import 'dart:collection';
+import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:booking_app/services/properties_class.dart';
@@ -7,14 +10,106 @@ class getpropertiesfromdatabase
   Stream<List<Properties>> get propers{
     return props.snapshots().map(_properlistfromsnapshots);
   }
+  List<Halls> tohallslist(List<dynamic> halls)
+  {
+    List<Halls> placehalls=[];
+    halls.forEach((element) {
+      LinkedHashMap e=element;
+      e.forEach((key, value) {
+        String hallname='';
+        int hallcap=0;
+        if(key=='hallname')
+          hallname=value;
+        else if(key=='hallCapacity')
+          hallcap=int.parse(value) ;
+        placehalls.add(Halls(hallName: hallname,hallCapacity: hallcap));
+
+
+
+      });
+    });
+    return placehalls;
+
+  }
+  List<section> tosectionslist(List<dynamic> sectionsj)
+  {
+    List<section> placesections=[];
+
+    sectionsj.forEach((element) {
+      LinkedHashMap e=element;
+      e.forEach((key, value) {
+        String sectionname='';
+        List<Room> prooms=[];
+        if(key=='sectionName')
+          sectionname=value;
+
+        else if(key=='sectionRooms')
+          {
+            List<dynamic> rooms=value;
+            rooms.forEach((element) {
+              LinkedHashMap roomsjson=element;
+
+              roomsjson.forEach((key, value) {
+                int numberofrooms=0;
+                String roomname='';
+                int cap=0;
+                if(key=='numberOfRooms')
+                  numberofrooms=int.parse(value);
+                else if(key=='roomName')
+                  roomname=value;
+                else if(key=='capacity')
+                  cap=int.parse(value);
+                prooms.add(Room(numOfRooms: numberofrooms,roomName: roomname,roomCapacity: cap));
+              });
+
+            });
+          }
+        placesections.add(section(sectionname: sectionname,rooms: prooms));
+
+
+
+      });
+    });
+    return placesections;
+
+  }
+
+
+
 
   List<Properties> _properlistfromsnapshots(QuerySnapshot snapshot)
   {
 
     return snapshot.docs.map((doc) {
-      return Properties(propID: doc.id, propName: doc.get('name')??'', location: doc.get('location')??'',ownerName:'anything');
+     List<dynamic> halls= doc.get('halls')??'';
+     List<dynamic> sections= doc.get('sections')??'';
+     List<Halls> placehalls=tohallslist(halls);
+     List<section> placesections=tosectionslist(sections);
+
+
+
+      return Properties(propID: doc.id, propName: doc.get('name')??'', location: doc.get('location')??'',ownerName:'anything',halls: placehalls,secs: placesections);
 
     }).toList();
+
+
+  }
+  void getproperties()
+  {
+    List<Properties> p=[];
+    props.where("name",isNotEqualTo: null).get().then((value) {
+      value.docs.forEach((element) {
+        print(element.data());
+        int x=3;
+
+
+      });
+      
+    });
+
+
+
+
   }
 
 
